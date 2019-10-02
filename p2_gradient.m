eaee4e703798dbc913977fd5177b639c5378fcda
dreamwarrior <farhanbuet09@gmail.com>
2019-10-01 21:29:07 -0400

    Project2 started finding gradient

---
+++ p2_gradient.m
@@ -0,0 +1,20 @@
+function g = p2_gradient(f,x,order,h)
+N=length(x);
+g=[];
+if order==2
+    for i=1:N
+        e=zeros(N,1);
+        e(i)=1;
+        %g(i)=
+        g(i)=(f(x+h*e)-f(x-h*e))/2*h;
+        
+    end
+
+else
+    for i=1:N
+        e=zeros(N);
+        e(i)=1;
+        g(i)=(f(x+h*e)-f(x))/h;
+    end
+end
+end

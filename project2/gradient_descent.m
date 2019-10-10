48f2d680677a8219e83e281defa9c538b84dfb02
dreamwarrior <farhanbuet09@gmail.com>
2019-10-10 10:00:35 -0400

    tolerance value added in parameter

--- project2/gradient_descent.m
+++ project2/gradient_descent.m
@@ -1,13 +1,22 @@
-function xk=gradient_descent(f,g,x0,alpha_init,no_of_iteration)
+function xk=gradient_descent(f,g,x0,alpha_init,f_tol,varargin)
 
+if nargin < 6
+    no_of_iteration = 1000;
+else
+    no_of_iteration = varargin{1};
+end
 %1. initialize xk and k
 xk = x0;
 k = 0;
 
 %2. Until x ( k ) is a good enough solution
-for i=0:no_of_iteration
+
+i=0;
+alpha=alpha_init;
+while i<no_of_iteration && abs(alpha)>f_tol
+    i=i+1;
     p=-g(xk);
-    alpha=backtrack_ls(f,g,p,xk,alpha_init);
+    alpha=backtrack_ls(f,g,p,xk,alpha);
     xk=xk+alpha*p;
     
 end

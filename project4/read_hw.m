52b43b8b1b24d37340a8d03ebfcb43ee2bba872f
dreamwarrior <farhanbuet09@gmail.com>
2019-12-08 00:16:23 -0500

    project 4 started

--- project4/read_hw.m
+++ project4/read_hw.m
@@ -1,4 +1,4 @@
-function [] = read_hw(type)
+function [image,label] = read_hw(typ)
 %output: image: nxN matrix, each column is an image
 % label: Nx1 vector, label for the digit for each imag
 fid = fopen('train-images-idx3-ubyte','r');
@@ -28,19 +28,24 @@ label = fread(fid2,60000,'uint8');
 % ln7 = length(find(label==7));
 % ln8 = length(find(label==8));
 % ln9 = length(find(label==9));
-
-image = zeros(2000,28*28);
-X=zeros(2000,28*28);
-for i=1:2000
-    image(i,:) = fread(fid,28*28,'uint8');
+rootpath="./att_faces";
+[image]=total_mat(rootpath);
+image=image';
+X=zeros(length(image),92*112);
+disp(size(image));
+for i=1:400
     meanX=mean(image(i,:));
     X(i,:)=image(i,:)-meanX;
 end
 X=X';
-%image = image';
+writematrix(image(1,:)'+" "+image(2,:)',"X_tab.txt",'Delimiter','tab');
+type 'X_tab.txt';
+image = image';
+fclose(fid);
+
 
 U=[];S=[];V=[];
-if strcmp(type,"svd")
+if strcmp(typ,"svd")
     [U,S,V] = svd(X);
     g=S*S';
     totalVariance=sum(sum(g));
@@ -66,7 +71,7 @@ if strcmp(type,"svd")
     Xnew=Unew*Snew*Vnew';
     Xnew=Xnew';
     for l=1:5
-        B=reshape(Xnew(l,:),[28,28])
+        B=reshape(Xnew(l,:),[92,112])
         figure;
         imshow(B, 'InitialMagnification', 'fit')
         title('4')
@@ -77,10 +82,10 @@ else
     [D,idx]=sort(sort(D,1,'descend'),2,'descend');
     D=diag(D(1,:));
     totalVariance=sum(sum(D));
-    sumSigma=zeros(784,1)
+    sumSigma=zeros(10304,1)
     sumS=0;
     K=0;
-    for k=1:784
+    for k=1:10304
         sumSigma(k)=sum(D(k,:));
         sumS=sumS+sumSigma(k);
         sumSigma(k)=sumS/totalVariance;
@@ -90,7 +95,7 @@ else
     end
     plot(sumSigma,"o")
     xlabel("total variance")
-    xlim([0 784])
+    xlim([0 1000])
     ylim([0 1])
     
     Vnew=V(:,1:K);
@@ -98,7 +103,7 @@ else
     Xnew=Vnew*Dnew*Vnew';
     Xnew=Xnew';
     for l=1:5
-        B=reshape(Xnew(l,:),[28,28])
+        B=reshape(Xnew(l,:),[92,112])
         figure;
         imshow(B, 'InitialMagnification', 'fit')
         title('4'+int2str(K))
@@ -106,6 +111,25 @@ else
     
 end
 
+end
 
 
-
+function [ t_mat ] = total_mat( root_path)
+totalsub=40;
+impersub=10;
+N = totalsub*impersub;
+M = 92*112;
+t_mat = zeros(M,N);
+msg = '';
+for s=1:totalsub
+    for i=1:10
+        nextPath = strcat(root_path, '/s', int2str(s), '/', int2str(i), '.pgm');
+        if exist(nextPath, 'file') 
+            nextImage = imread(nextPath, 'PGM');
+                t_mat(:,(s-1)*impersub+i) = nextImage ( : );
+        else
+            disp(strcat(msg,'image [', nextPath,'] does not exist.\n'));
+        end
+    end
+end
+end
